ff6f9449a25bfc891364ab652c3534ecb452be77
LewisJassy <jassylewis70@gmail.com>
2025-07-09 17:38:07 +0300

    Fix syntax error in step00b_syntax_error and add user object logging in step00_helloworld

--- step00_helloworld/app.ts
+++ step00_helloworld/app.ts
@@ -2,3 +2,6 @@ let message = "Hello World";// Infering Types,
                             //take your cursor on the variable name
 console.log(message);
         
+let user = {number: 1, name: "lewis", email: "lewis@gmail.com"}
+let email = user.email
+ console.log(email)
